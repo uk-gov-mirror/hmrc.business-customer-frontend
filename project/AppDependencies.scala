86bffe1dd5d1d3947e7f4a12ac6f3923cc639486
Simon Bailey <5601214+thebailers@users.noreply.github.com>
2025-11-07 17:32:10 +0000

    DL-17790 - upgrade deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,14 +19,14 @@ import sbt.*
 object AppDependencies {
   import play.sbt.PlayImport.ws
 
-  private val bootstrapPlayVersion = "10.3.0"
+  private val bootstrapPlayVersion = "10.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"  %% "bootstrap-frontend-play-30"   % bootstrapPlayVersion,
     "uk.gov.hmrc"  %% "domain-play-30"               % "11.0.0",
     "uk.gov.hmrc"  %% "play-partials-play-30"        % "10.2.0",
-    "uk.gov.hmrc"  %% "play-frontend-hmrc-play-30"   % "12.19.0",
+    "uk.gov.hmrc"  %% "play-frontend-hmrc-play-30"   % "12.20.0",
     "uk.gov.hmrc"  %% "http-caching-client-play-30"  % "12.2.0",
   )
 
